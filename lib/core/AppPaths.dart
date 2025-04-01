deec4b19bb2d7697bb6e2d1d3b1a6fbe6ae2ccff
tdluong <tranducluongdba@gmail.com>
2025-04-01 21:02:22 +0700

    [feat] Implement authentication with BLoC and Firebase

---
+++ lib/core/AppPaths.dart
@@ -0,0 +1,11 @@
+class AppPaths {
+  static const String dashboardPath = '/';
+  static const String transactionsPath = '/transactions';
+  static const String addTransactionPath = '/transactions/add';
+  static const String editTransactionPath = '/transactions/:id';
+  static const String categoriesPath = '/categories';
+  static const String settingsPath = '/settings';
+  static const String reportsPath = '/reports';
+  static const String loginPath = '/login';
+  static const String transactionListPath = '/transaction-list';
+}
